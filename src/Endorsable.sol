3b688e95f36c6f1a4caa17bf04f8f0d7b8bb49aa
Bruce Donovan <bruce.donovan@gmail.com>
2025-02-10 13:04:56 +0000

    refactor: replace uint8 with enum for endorsement states for improved readability

--- src/Endorsable.sol
+++ src/Endorsable.sol
@@ -23,15 +23,17 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Endorsable is Ownable {
-    mapping(address => uint8) private endorsements;
+    mapping(address => endorseState) private endorsements;
 
+    /// @dev Enum representing the state of the endorsement state.
+    /// @notice We could have used uint8 to save minor gas, but instead opted for the more readable and self-documenting string option.
     enum endorseState {
-        UNASSIGNED, // 0
-        REQUESTED, // 1
-        ENDORSED, // 2
-        REVOKED, // 3
-        REMOVED // 4
-            // BLACKLISTED // 5
+        UNASSIGNED, 
+        REQUESTED,
+        ENDORSED,
+        REVOKED,
+        REMOVED
+        // BLACKLISTED
     }
 
     event Endorsed(address indexed endorser);
@@ -48,52 +50,52 @@ contract Endorsable is Ownable {
     /// @notice tx sender endorses the contract, ONLY if requested to do so.
     /// @dev endorses a contract
     function endorse() external {
-        require(endorsements[msg.sender] == uint8(endorseState.REQUESTED), "Endorsement not requested.");
-        endorsements[msg.sender] = uint8(endorseState.ENDORSED);
+        require(endorsements[msg.sender] == endorseState.REQUESTED, "Endorsement not requested.");
+        endorsements[msg.sender] = endorseState.ENDORSED;
         emit Endorsed(msg.sender);
     }
 
     /// @notice tx sender revokes thier address from endorsements
     /// @dev if not endorsed yet, it remains in the list as requested
     function revokeEndorsement() external {
-        require(endorsements[msg.sender] == uint8(endorseState.ENDORSED), "Not endorsed, already revoked, or removed.");
-        endorsements[msg.sender] = uint8(endorseState.REVOKED);
+        require(endorsements[msg.sender] == endorseState.ENDORSED, "Not endorsed, already revoked, or removed.");
+        endorsements[msg.sender] = endorseState.REVOKED;
         emit EndorsementRevoked(msg.sender);
     }
 
     /// @dev Adds an address to the signatures requested
     /// Note: this will revert 'Removed' and 'Revoked' statuses back to 'Requested'.
     function requestEndorsement(address addr) external onlyOwner {
-        require(endorsements[addr] != uint8(endorseState.ENDORSED), "Already endorsed.");
-        require(endorsements[addr] != uint8(endorseState.REQUESTED), "Already requested.");
+        require(endorsements[addr] != endorseState.ENDORSED, "Already endorsed.");
+        require(endorsements[addr] != endorseState.REQUESTED, "Already requested.");
         // require(
-        //     endorsements[addr] != uint8(endorseState.BLACKLISTED),
+        //     endorsements[addr] != endorseState.BLACKLISTED,
         //     "Cannot request from blacklisted address."
         // );
-        endorsements[addr] = uint8(endorseState.REQUESTED);
+        endorsements[addr] = endorseState.REQUESTED;
         emit EndorsementRequested(addr);
     }
 
     /// @dev removes any address that has previously signed the item
     function removeEndorsement(address addr) external onlyOwner {
         require(
-            endorsements[addr] == uint8(endorseState.ENDORSED) || endorsements[addr] == uint8(endorseState.REQUESTED),
+            endorsements[addr] == endorseState.ENDORSED || endorsements[addr] == endorseState.REQUESTED,
             "Not endorsed or requested."
         );
-        endorsements[addr] = uint8(endorseState.REMOVED);
+        endorsements[addr] = endorseState.REMOVED;
         emit EndorsementRemoved(addr);
     }
 
     // /// @dev Blacklists an address, preventing future endorsements ( - IS THIS EVEN NEEDED? )
     // function blacklistAddress(address addr) external onlyOwner {
-    //     endorsements[addr] = uint8(endorseState.BLACKLISTED);
+    //     endorsements[addr] = endorseState.BLACKLISTED;
     //     emit Blacklisted(addr);
     // }
 
     /// Returns if the signature is requested.
     /// @dev retrieves the state of the endorsement
     /// @return endorseState representing the endorsement state of the contract
-    function getEndorsementStatus(address addr) public view returns (uint8) {
+    function getEndorsementStatus(address addr) public view returns (endorseState) {
         return endorsements[addr];
     }
 
